3cfb3ca0b2480694a682c0bdc3640794749567a5
candariconrado-design <candariconrado@gmail.com>
2026-09-09 09:42:13 +0800

    Update working Supabase API key

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
 const SUPABASE_URL = 'https://agydqpurvxwvfhcaluj.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFneWRxcHVydnh3dmZ2aGNhbHVqIiwicm9sZSI6ImFno24iLCJpYXQiOjE3ODg4OTA3MzYsImV4cCI6MjEwNDQ2NjczNn0.BUxfVuaw808RkhichBEVxJiMLX4VkyUAOfJqTmAq9ws';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFneWRxcHVydnh3dmZ2aGNhbHVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg4OTA3MzYsImV4cCI6MjEwNDQ2NjczNn0.BUxfVuaw808RkhichBEVxJiMLX4VkyUAOfJqTmAq9ws';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
